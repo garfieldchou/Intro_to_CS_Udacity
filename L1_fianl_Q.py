067a1166be56aee9d1270a2fddc064f69b87ec76
shaudai <shaudai0402@hotmail.com>
2015-07-11 10:53:01 +0800

    Remove debug 'print' and add a comment for manipulating find() method

--- L1_fianl_Q.py
+++ L1_fianl_Q.py
@@ -12,13 +12,7 @@ page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
 # print page
 start_link = page.find('<a href=')
-# print 'start_link= ', start_link
-# print 'after start_link= ', page[start_link:]
 first_quote = page.find('"', start_link)
-# print 'first quote= ', first_quote
-# print 'after first_quote= ', page[first_quote:]
-second_quote = page.find('"', first_quote+1)
-# print 'second quote= ', second_quote
-# print 'after second_quote= ', page[second_quote:]
+second_quote = page.find('"', first_quote+1) # start with 'first_quote+1' instead of 'first_quote'
 url = page[first_quote+1 : second_quote]
 print url
