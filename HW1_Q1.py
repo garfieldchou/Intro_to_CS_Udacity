c581c4e834e86fce69a2381cecd849e635a07fba
shaudai <shaudai0402@hotmail.com>
2015-07-11 11:51:28 +0800

    Fix "SyntaxError: EOL while scanning string literal" with """

--- HW1_Q1.py
+++ HW1_Q1.py
@@ -1,7 +1,8 @@
 # Search Engines And The Web
 
 # sample page
-page = '<html xmlns="http://www.w3.org/1999/xhtml">
+page = """
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>Udacity</title>
 </head>
@@ -11,7 +12,8 @@ page = '<html xmlns="http://www.w3.org/1999/xhtml">
 <a href="http://www.wikipedia.org/wiki/Higher_education">higher education founded by</a> <a href="http://www.wikipedia.org/wiki/Sebastian_Thrun">Sebastian Thrun</a>, David Stavens, and Mike Sokolsky with the goal to provide university-level education that is "both high quality and low cost".</p>   
 <p> It is the outgrowth of a free computer science class offered in 2011 through Stanford University. Currently, Udacity is working on its second course on building a search engine. Udacity was announced at the 2012 <a href="http://www.wikipedia.org/wiki/Digital_Life_Design">Digital Life Design</a> conference.</p>      
 </body>
-</html>'
+</html>
+"""
 
 while (start_link = page.find('<a href=')) != -1:
     # start_link = page.find('<a href=')
